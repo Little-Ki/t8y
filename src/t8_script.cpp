d4651072c89295e0b36d7723353aaa439df26bda
Little-Ki <970559705@qq.com>
2025-03-19 16:56:24 +0800

    update

--- src/t8_script.cpp
+++ src/t8_script.cpp
@@ -4,11 +4,11 @@ namespace t8 {
 
     std::string _script;
 
-    void t8::script_set(std::string &text) {
+    void script_set(std::string &text) {
         _script = text;
     }
     
-    std::string &t8::script_get() {
+    std::string &script_get() {
         return _script;
     }
     
