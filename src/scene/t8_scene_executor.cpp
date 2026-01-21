c7fae5adca16cadfb89240951009477305b8cb64
Little-Ki <970559705@qq.com>
2026-01-21 10:45:41 +0800

    update

--- src/scene/t8_scene_executor.cpp
+++ src/scene/t8_scene_executor.cpp
@@ -18,20 +18,24 @@ using namespace t8::utils;
 #define ASSERT_EXECUTE(x)                                       \
     {                                                           \
         auto r = (x);                                           \
-        if (!r.valid()) {                                       \
+        if (!r.valid())                                         \
+        {                                                       \
             sol::error err = r;                                 \
             ctx_signals().push({SIGNAL_EXCEPTION, err.what()}); \
             return;                                             \
         }                                                       \
     }
 
-namespace t8::scene {
-    struct ExecutorState {
+namespace t8::scene
+{
+    struct ExecutorState
+    {
         uint8_t select;
         bool paused{false};
     };
 
-    struct ScriptVM {
+    struct ScriptVM
+    {
         sol::state lua;
         sol::protected_function init;
         sol::protected_function update;
@@ -42,74 +46,93 @@ namespace t8::scene {
 
     static std::optional<ScriptVM> vm;
 
-    void setup_vm_api(sol::state &lua) {
+    void setup_vm_api(sol::state &lua)
+    {
         lua.set_function(
             "clip",
             sol::overload(
-                [](int x, int y, int w, int h) { painter_clip(x, y, w, h); },
-                []() { painter_clip(); }));
+                [](int x, int y, int w, int h)
+                { painter_clip(x, y, w, h); },
+                []()
+                { painter_clip(); }));
 
         lua.set_function(
             "pal",
             sol::overload(
-                [](uint8_t a, uint8_t b) { painter_palette(a, b); },
-                [](uint8_t a) { return painter_palette(a); }));
+                [](uint8_t a, uint8_t b)
+                { painter_palette(a, b); },
+                [](uint8_t a)
+                { return painter_palette(a); }));
 
         lua.set_function(
             "palt",
             sol::overload(
-                [](uint8_t c, bool b) { painter_palette_mask(c, b); },
-                [](std::optional<uint16_t> m) { painter_palette_mask(m.value_or(0)); }));
+                [](uint8_t c, bool b)
+                { painter_palette_mask(c, b); },
+                [](std::optional<uint16_t> m)
+                { painter_palette_mask(m.value_or(0)); }));
 
         lua.set_function(
             "cls",
-            [](std::optional<uint8_t> c) { painter_clear(c.value_or(0)); });
+            [](std::optional<uint8_t> c)
+            { painter_clear(c.value_or(0)); });
 
         lua.set_function(
             "pget",
-            [](int x, int y) { return painter_pixel(x, y); });
+            [](int x, int y)
+            { return painter_pixel(x, y); });
 
         lua.set_function(
             "pset",
-            [](int x, int y, uint8_t c) { painter_pixel(x, y, c); });
+            [](int x, int y, uint8_t c)
+            { painter_pixel(x, y, c); });
 
         lua.set_function(
             "fget",
-            [](uint8_t i) { painter_flags(i); });
+            [](uint8_t i)
+            { painter_flags(i); });
 
         lua.set_function(
             "fset",
-            [](uint8_t i, uint8_t f) { painter_flags(i, f); });
+            [](uint8_t i, uint8_t f)
+            { painter_flags(i, f); });
 
         lua.set_function(
             "sget",
-            [](int x, int y) { return painter_sprite(x, y); });
+            [](int x, int y)
+            { return painter_sprite(x, y); });
 
         lua.set_function(
             "sset",
-            [](int x, int y, uint8_t c) { painter_sprite(x, y, c); });
+            [](int x, int y, uint8_t c)
+            { painter_sprite(x, y, c); });
 
         lua.set_function(
             "mget",
-            [](int x, int y) { return painter_map(x, y); });
+            [](int x, int y)
+            { return painter_map(x, y); });
 
         lua.set_function(
             "mset",
-            [](int x, int y, uint8_t i) { painter_map(x, y, i); });
+            [](int x, int y, uint8_t i)
+            { painter_map(x, y, i); });
 
         lua.set_function(
             "line",
-            [](int x0, int y0, int x1, int y1, uint8_t c) { painter_line(x0, y0, x1, y1, c); });
+            [](int x0, int y0, int x1, int y1, uint8_t c)
+            { painter_line(x0, y0, x1, y1, c); });
 
         lua.set_function(
             "cric",
-            [](int x, int y, int r, int c, std::optional<bool> f) {
+            [](int x, int y, int r, int c, std::optional<bool> f)
+            {
                 painter_circle(x, y, r, c, f.value_or(false));
             });
 
         lua.set_function(
             "rect",
-            [](int x, int y, int w, int h, uint8_t c, std::optional<bool> f) {
+            [](int x, int y, int w, int h, uint8_t c, std::optional<bool> f)
+            {
                 painter_rect(x, y, w, h, c, f.value_or(false));
             });
 
@@ -118,57 +141,71 @@ namespace t8::scene {
             [](
                 std::string s, int x, int y,
                 std::optional<uint8_t> _c,
-                std::optional<int> _w0, std::optional<int> _w1) {
+                std::optional<int> _w0, std::optional<int> _w1)
+            {
                 const auto w0 = _w0.value_or(4);
                 const auto w1 = _w1.value_or(8);
                 const auto c = _c.value_or(1);
                 const auto sx = x;
-                for (auto &ch : s) {
-                    if (ch == '\n') {
+                for (auto &ch : s)
+                {
+                    if (ch == '\n')
+                    {
                         y += 8;
                         x = sx;
-                    } else if (ch != '\r') {
+                    }
+                    else if (ch != '\r')
+                    {
                         painter_char(ch, x, y, c, true);
-                        x += ch < 0x80 ? w0 : w1;
+                        x += (static_cast<uint8_t>(ch) < 0x80) ? w0 : w1;
                     }
                 }
             });
 
         lua.set_function(
             "btn",
-            [](uint8_t i) { return gamepad_down(i >> 3, 1 << (i & 0b111)); });
+            [](uint8_t i)
+            { return gamepad_down(i >> 3, 1 << (i & 0b111)); });
 
         lua.set_function(
             "btnp",
-            [](uint8_t i) { return gamepad_pressed(i >> 3, 1 << (i & 0b111)); });
+            [](uint8_t i)
+            { return gamepad_pressed(i >> 3, 1 << (i & 0b111)); });
 
         lua.set_function(
             "key",
-            [](uint8_t k) { return keybd_down(k); });
+            [](uint8_t k)
+            { return keybd_down(k); });
 
         lua.set_function(
             "keyp",
-            [](uint8_t k) { return keybd_pressed(k); });
+            [](uint8_t k)
+            { return keybd_pressed(k); });
 
         lua.set_function(
             "mouse",
-            []() { return std::make_tuple(mouse_x(), mouse_y(), mouse_z(), mouse_button()); });
+            []()
+            { return std::make_tuple(mouse_x(), mouse_y(), mouse_z(), mouse_button()); });
 
         lua.set_function(
             "log",
-            [](std::string s) { ctx_signals().push({SIGNAL_PRINT, s}); });
+            [](std::string s)
+            { ctx_signals().push({SIGNAL_PRINT, s}); });
 
         lua.set_function(
             "time",
-            []() { return timer_ticks(); });
+            []()
+            { return timer_ticks(); });
 
         lua.set_function(
             "tstamp",
-            []() { return std::chrono::seconds(std::time(NULL)); });
+            []()
+            { return std::chrono::seconds(std::time(NULL)); });
 
         lua.set_function(
             "spr",
-            [](uint8_t id, int x, int y, std::optional<int> _s, std::optional<int> _f, std::optional<int> _r) {
+            [](uint8_t id, int x, int y, std::optional<int> _s, std::optional<int> _f, std::optional<int> _r)
+            {
                 const auto s = _s.value_or(0);
                 const auto f = _f.value_or(0);
                 const auto r = _r.value_or(0);
@@ -177,8 +214,10 @@ namespace t8::scene {
                 auto sprite_x = (id & 0xF) << 3;
                 auto sprite_y = ((id >> 4) & 0xF) << 3;
 
-                for (auto dy = 0; dy < 8; dy++) {
-                    for (auto dx = 0; dx < 8; dx++) {
+                for (auto dy = 0; dy < 8; dy++)
+                {
+                    for (auto dx = 0; dx < 8; dx++)
+                    {
                         auto tx = dx;
                         auto ty = dy;
 
@@ -188,13 +227,15 @@ namespace t8::scene {
                         if (f & 0b10)
                             ty = 7 - ty;
 
-                        if (r & 0b1) {
+                        if (r & 0b1)
+                        {
                             auto t = x;
                             tx = ty;
                             ty = 7 - t;
                         }
 
-                        if (r & 0b10) {
+                        if (r & 0b10)
+                        {
                             tx = 7 - tx;
                             ty = 7 - ty;
                         }
@@ -208,19 +249,22 @@ namespace t8::scene {
 
         lua.set_function(
             "map",
-            [](int mx, int my, int mw, int mh, int sx, int sy, std::optional<int> _scale, std::optional<uint8_t> _layers) {
+            [](int mx, int my, int mw, int mh, int sx, int sy, std::optional<int> _scale, std::optional<uint8_t> _layers)
+            {
                 const auto scale = std::clamp(_scale.value_or(1), 1, 4);
                 const auto layers = _layers.value_or(0xff);
                 const auto chunk_size = 8 * scale;
                 const auto pixel_size = scale;
 
-                if (mx < 0) {
+                if (mx < 0)
+                {
                     sx -= mx * chunk_size;
                     mw += mx;
                     mx = 0;
                 }
 
-                if (my < 0) {
+                if (my < 0)
+                {
                     sy -= my * chunk_size;
                     mh += my;
                     my = 0;
@@ -234,8 +278,10 @@ namespace t8::scene {
                 auto r = std::clamp(mx + mw, 0, 128);
                 auto b = std::clamp(my + mh, 0, 128);
 
-                for (auto y = t; y < b; y++) {
-                    for (auto x = l; x < r; x++) {
+                for (auto y = t; y < b; y++)
+                {
+                    for (auto x = l; x < r; x++)
+                    {
                         auto id = painter_map(x, y);
 
                         if (!(id & layers))
@@ -244,8 +290,10 @@ namespace t8::scene {
                         auto sprite_x = (id & 0xF) << 3;
                         auto sprite_y = ((id >> 4) & 0xF) << 3;
 
-                        for (auto dy = 0; dy < 8; dy++) {
-                            for (auto dx = 0; dx < 8; dx++) {
+                        for (auto dy = 0; dy < 8; dy++)
+                        {
+                            for (auto dx = 0; dx < 8; dx++)
+                            {
                                 auto color = painter_sprite(sprite_x + dx, sprite_y + dy);
                                 painter_rect(
                                     sx + x * chunk_size + dx * pixel_size,
@@ -258,52 +306,69 @@ namespace t8::scene {
             });
     }
 
-    void executor_update() {
-        if (keybd_pressed(41) && !state.paused) {
+    void executor_update()
+    {
+        if (keybd_pressed(41) && !state.paused)
+        {
             state.paused = true;
             state.select = 0;
         }
 
-        if (state.paused) {
-            if (keybd_pressed(82) && state.select) {
+        if (state.paused)
+        {
+            if (keybd_pressed(82) && state.select)
+            {
                 state.select -= 1;
             }
-            if (keybd_pressed(81)) {
+            if (keybd_pressed(81))
+            {
                 state.select = std::clamp(state.select + 1, 0, 1);
             }
-            if (keybd_pressed(40) || keybd_pressed(88)) {
+            if (keybd_pressed(40) || keybd_pressed(88))
+            {
                 state.paused = false;
-                if (state.select == 1) {
+                if (state.select == 1)
+                {
                     ctx_signals().push({SIGNAL_SWAP_CONSOLE});
                 }
             }
         }
 
-        if (!state.paused) {
-            if (vm->update) {
+        if (!state.paused)
+        {
+            if (vm->update)
+            {
                 ASSERT_EXECUTE(vm->update());
             }
         }
     }
 
-    void executor_draw() {
+    void executor_draw()
+    {
         painter_clear(0);
 
-        if (vm->draw) {
+        if (vm->draw)
+        {
             ASSERT_EXECUTE(vm->draw());
         }
 
-        if (state.paused) {
+        if (state.paused)
+        {
             static const std::string menu[] = {"RESUME", "EXIT"};
 
-            for (auto i = 0; i < 2; i++) {
+            for (auto i = 0; i < 2; i++)
+            {
                 auto x = 1;
                 auto y = 110 + 9 * i;
 
-                for (const auto &ch : menu[i]) {
-                    if (state.select == i) {
+                for (const auto &ch : menu[i])
+                {
+                    if (state.select == i)
+                    {
                         painter_char(ch, x, y, 3);
-                    } else {
+                    }
+                    else
+                    {
                         painter_char(ch, x, y, 1);
                     }
                     x += 4;
@@ -312,7 +377,8 @@ namespace t8::scene {
         }
     }
 
-    void executor_enter() {
+    void executor_enter()
+    {
         swap_memory(true);
 
         vm.reset();
@@ -326,7 +392,8 @@ namespace t8::scene {
         vm->update = vm->lua["update"];
         vm->draw = vm->lua["draw"];
 
-        if (vm->init) {
+        if (vm->init)
+        {
             ASSERT_EXECUTE(vm->init());
         };
 
@@ -337,7 +404,8 @@ namespace t8::scene {
         timer_reset();
     }
 
-    void executor_leave() {
+    void executor_leave()
+    {
         swap_memory(false);
     }
 
